6ef814c23bc2d98292e83158729fa66c45c637e0
npax1 <prakh55555@gmail.com>
2022-10-01 10:18:11 +0300

    print code+-

--- ex_1.py
+++ ex_1.py
@@ -1,8 +1,8 @@
 start=int(input("веди число і"))
 end=int(input("веди число"))
-while start <= end:
+while start => end:
     print(start)
     start=start+1
-    if start/2
+    if start/2:
         start = +1
-
+        print(start)
